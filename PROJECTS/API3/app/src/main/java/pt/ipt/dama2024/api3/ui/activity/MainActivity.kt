424533f292b159258b91a7f760037292761968f1
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2024-04-18 21:49:29 +0100

    cards organization inside main view

--- PROJECTS/API3/app/src/main/java/pt/ipt/dama2024/api3/ui/activity/MainActivity.kt
+++ PROJECTS/API3/app/src/main/java/pt/ipt/dama2024/api3/ui/activity/MainActivity.kt
@@ -2,11 +2,57 @@ package pt.ipt.dama2024.api3.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import pt.ipt.dama2024.api3.R
+import pt.ipt.dama2024.api3.model.Note
+import pt.ipt.dama2024.api3.retrofit.RetrofitIniti
+import pt.ipt.dama2024.api3.ui.adapter.NoteListAdapter
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        listNotes()
+    }
+
+    private fun listNotes() {
+        //retrieve API data
+        val call = RetrofitIniti().noteService().getNotes()
+        // do something with the data
+        processList(call)
+    }
+
+    /**
+     * Proccessing list which was received after using API
+     */
+    private fun processList(call: Call<List<Note>>) {
+        call.enqueue(object : Callback<List<Note>?> {
+            override fun onResponse(call: Call<List<Note>?>?,
+                                    response: Response<List<Note>?>?) {
+                response?.body()?.let {
+                    val notes: List<Note> = it
+                    configureList(notes)
+                }
+            }
+            override fun onFailure(call: Call<List<Note>?>?, t: Throwable?) {
+                t?.printStackTrace()
+                t?.message?.let { Log.e("onFailure error", it) }
+            }
+        })
+    }
+
+    private fun configureList(notes: List<Note>) {
+        val recyclerView: RecyclerView = findViewById(R.id.noteList)
+        recyclerView.adapter = NoteListAdapter(notes, this)
+        val layoutManager = StaggeredGridLayoutManager(
+            2, StaggeredGridLayoutManager.VERTICAL
+        )
+        recyclerView.layoutManager = layoutManager
     }
 }
