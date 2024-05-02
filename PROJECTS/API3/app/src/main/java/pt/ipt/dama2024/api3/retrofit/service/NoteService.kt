3b9305c275f0e6ce769d9081fef13d8a330ac489
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2024-05-02 19:29:20 +0100

    adding a new api route which uses POST method to send a new POST object

--- PROJECTS/API3/app/src/main/java/pt/ipt/dama2024/api3/retrofit/service/NoteService.kt
+++ PROJECTS/API3/app/src/main/java/pt/ipt/dama2024/api3/retrofit/service/NoteService.kt
@@ -2,7 +2,9 @@ package pt.ipt.dama2024.api3.retrofit.service
 
 import pt.ipt.dama2024.api3.model.Note
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 /**
  * function description that fetch data from API
@@ -18,4 +20,13 @@ interface NoteService {
      */
     @GET("api/notes")
     fun getNotes(): Call<List<Note>>
+
+
+    /**
+     * Post a note to the API route api/notes
+     *
+     * @param note
+     */
+    @POST("api/notes")
+    fun addNote(@Body note:Note):Call<Note>
 }
